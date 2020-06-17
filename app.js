3274d1dcac62a7f4276cab47dbe45124d6b23271
Fowley <alexanderfowle@gmail.com>
2020-06-16 21:29:57 -0500

    Install add'l dependencies & testing with Node

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Starting!');
+// npm packages
+const dotenv = require('dotenv').config();
+const express = require('express');
+const app = express();
+
+// Define middleware
+app.use(express.json());
+
+// Environment variables
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Testing!!!!');
+});
+
+app.get('/api/users', (req, res) => {
+    res.send([1, 2, 3, 4]);
+});
+
+app.get('/api/users/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
