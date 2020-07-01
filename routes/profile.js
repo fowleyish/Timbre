05efb0ba0b3271e6eb2a8b30968fd80606fc185d
Fowley <alexanderfowle@gmail.com>
2020-07-01 12:08:50 -0500

    Added functionality to follow a user

--- routes/profile.js
+++ routes/profile.js
@@ -18,4 +18,25 @@ router.get('/:id', ensureAuth, (req, res) => {
     .catch(err => console.log(err));
 });
 
+router.get('/follow/:thisUserId', (req, res) => {
+    const userToFollow = req.params.thisUserId;
+    const currentUser = req.user;
+    if (currentUser.Following == null) {
+        const followingList = [userToFollow];
+        const serializedFollowingList = JSON.stringify(followingList);
+        User.update({ Following: serializedFollowingList }, { where: { UserId: currentUser.UserId } })
+        .then(user => console.log('You are now following this user!'))
+        .then(user => res.redirect('localhost:3000/profile/'+userToFollow))
+        .catch(err => console.log(err));
+    } else {
+        const parsedFollowingList = JSON.parse(currentUser.Following);
+        parsedFollowingList.push(userToFollow);
+        const stringifiedFollowingList = JSON.stringify(parsedFollowingList);
+        User.update({ Following: stringifiedFollowingList }, { where: { UserId: currentUser.UserId } })
+        .then(user => console.log('You are now following this user!'))
+        .then(user => res.redirect('localhost:3000/profile/'+userToFollow))
+        .catch(err => console.log(err));
+    }
+});
+
 module.exports = router;
