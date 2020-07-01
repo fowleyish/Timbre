fd9e57ca121fd35f58be62a24bc5bc4a3988e7ca
Fowley <alexanderfowle@gmail.com>
2020-07-01 11:07:54 -0500

    Updated profile view to differ signed-in user from target user; added nav personalization for signed-in user

--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuth } = require('../config/auth');
+const User = require('../models/index').user;
 
 router.get('/:id', ensureAuth, (req, res) => {
-    res.render('profile', {
-        user: req.user,
-        id: id
-    });
+    User.findOne({
+        where: {
+            UserId: req.params.id
+        }
+    })
+    .then(targetUser => {
+        res.render('profile', {
+            user: req.user,
+            thisUser: targetUser
+        });
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
