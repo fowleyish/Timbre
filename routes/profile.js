29032e2cf6b63524f5ae96f8a64b9bdf7dd434e7
Fowley <alexanderfowle@gmail.com>
2020-07-01 12:26:06 -0500

    Added functionality to unfollow a user

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuth } = require('../config/auth');
+const { parse } = require('path');
 const User = require('../models/index').user;
 
 router.get('/:id', ensureAuth, (req, res) => {
@@ -37,6 +38,18 @@ router.get('/follow/:thisUserId', (req, res) => {
         .then(user => res.redirect('localhost:3000/profile/'+userToFollow))
         .catch(err => console.log(err));
     }
+});
+
+router.get('/unfollow/:thisUserId', (req, res) => {
+    const userToUnfollow = req.params.thisUserId;
+    const currentUser = req.user;
+    const parsedFollowingList = JSON.parse(currentUser.Following);
+    const filteredFollowingList = parsedFollowingList.filter(x => x !== userToUnfollow);
+    const stringifiedFollowingList = JSON.stringify(filteredFollowingList);
+    User.update({ Following: stringifiedFollowingList }, { where: { UserId: currentUser.UserId } })
+    .then(user => console.log('You are now following this user!'))
+    .then(user => res.redirect('localhost:3000/profile/'+userToFollow))
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
