9efcc80bd744404f285e5d615695447fc5aecca6
Fowley <alexanderfowle@gmail.com>
2020-07-05 23:25:21 -0500

    Added user following call to dashboard; will use to build components

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuth } = require('../config/auth');
+const axios = require('axios');
+const User = require('../models/index').user;
 
 router.get('/', ensureAuth, (req, res) => {
     if (req.user.SpotifyRefreshToken == null) {
@@ -9,9 +11,29 @@ router.get('/', ensureAuth, (req, res) => {
             user: req.user
         })
     } else {
-        res.render('dashboard', {
-            user: req.user
-        });
+        if (req.user.Following !== null) {
+            const followingList = JSON.parse(req.user.Following);
+            let followingListPromises = [];
+            for(let i=0; i<followingList.length; i++) {
+                let userProm = User.findOne({
+                    where: {
+                        UserId: followingList[i]
+                    }
+                })
+                followingListPromises.push(userProm);
+            }
+            Promise.all(followingListPromises)
+            .then((following) => {
+                res.render('dashboard', {
+                    user: req.user,
+                    following: following
+                });
+            })
+        } else {
+            res.render('dashboard', {
+                user: req.user
+            });
+        }
     }
 });
 
