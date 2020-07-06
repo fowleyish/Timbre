8f023ff0d4ebc9237e65eadeca15fa55163013f9
Fowley <alexanderfowle@gmail.com>
2020-07-06 00:40:13 -0500

    Added dashboard feed UI

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,7 +4,7 @@ const { ensureAuth } = require('../config/auth');
 const axios = require('axios');
 const User = require('../models/index').user;
 
-router.get('/', ensureAuth, (req, res) => {
+router.get('/', ensureAuth, async (req, res) => {
     if (req.user.SpotifyRefreshToken == null) {
         console.log(spotAuthUrl);
         res.render('setup', {
@@ -22,13 +22,33 @@ router.get('/', ensureAuth, (req, res) => {
                 })
                 followingListPromises.push(userProm);
             }
-            Promise.all(followingListPromises)
-            .then((following) => {
-                res.render('dashboard', {
-                    user: req.user,
-                    following: following
-                });
-            })
+            const resolvedFollowingList = await Promise.all(followingListPromises);
+            let recentlyPlayedAll = [];
+            for(let i=0; i<resolvedFollowingList.length; i++) {
+                let thisRecentlyPlayed = axios({
+                    url: 'https://api.spotify.com/v1/me/player/recently-played?limit=10',
+                    method: 'get',
+                    headers: {
+                        'Accept':'application/json',
+                        'Content-Type':'application/json',
+                        'Authorization':'Bearer ' + resolvedFollowingList[i]._previousDataValues.SpotifyToken
+                    }
+                })
+                recentlyPlayedAll.push(thisRecentlyPlayed);
+            }
+            const resolvedRecentlyPlayed = await Promise.all(recentlyPlayedAll);
+            let recentlyPlayedTotal = [];
+            for(let i=0; i<resolvedRecentlyPlayed.length; i++) {
+                for(let j=0; j<resolvedRecentlyPlayed[i].data.items.length; j++) {
+                    recentlyPlayedTotal.push(resolvedRecentlyPlayed[i].data.items[j]);
+                }
+            }
+
+            res.render('dashboard', {
+                user: req.user,
+                following: resolvedFollowingList,
+                feed: recentlyPlayedTotal
+            });
         } else {
             res.render('dashboard', {
                 user: req.user
